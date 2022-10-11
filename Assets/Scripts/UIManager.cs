08d0444c018e52ffcfae61ddec2a166fa4d46d03
allenenewgate <115376306+allenenewgate@users.noreply.github.com>
2022-10-11 17:10:47 -0500

    title screen
    - added title text
    - added instructions/start button
    - added instructions screen when pressed

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject instructions;
+    [SerializeField]
+    private GameObject titleScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HideInstructions();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowInstructions()
+    {
+        instructions.SetActive(true);
+        titleScreen.SetActive(false);
+    }
+
+    private void HideInstructions()
+    {
+        instructions.SetActive(false);
+        titleScreen.SetActive(true);
+    }
+}
